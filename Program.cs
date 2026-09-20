772ae7484155e17bdc25515ef3389570587e4bcd
Anastasiia <hociy2015@gmail.com>
2026-09-20 15:40:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+ 
+namespace MyTeamCityProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, TeamCity!");
+        }
+    }
+}
