44fa4b22257cd124f02d65168b1232ab51065390
Anastasiia <hociy2015@gmail.com>
2026-09-20 15:49:09 +0300

    Test auto trigger

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyTeamCityProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, TeamCity!");
+            Console.WriteLine("Hello, TeamCity!:)");
         }
     }
 }
